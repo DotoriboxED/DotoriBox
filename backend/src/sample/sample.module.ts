42aad6c07300eead1a0d0ad3f4fdb9f872818228
Song <songyw9812@gmail.com>
2021-08-17 16:04:42 +0900

    add sampleModule

--- backend/src/sample/sample.module.ts
+++ backend/src/sample/sample.module.ts
@@ -1,4 +1,13 @@
 import { Module } from '@nestjs/common';
+import { SampleController } from './sample.controller';
+import { SampleService } from './sample.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Sample } from './entity/sample.entity';
+import { Stock } from '../stock/stock.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Sample, Stock])],
+  controllers: [SampleController],
+  providers: [SampleService],
+})
 export class SampleModule {}
