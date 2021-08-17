032009adb2b109fc489eb391415538c46c07439f
Song <songyw9812@gmail.com>
2021-08-17 16:03:07 +0900

    fix stockModule Dependencies Error

--- backend/src/stock/stock.module.ts
+++ backend/src/stock/stock.module.ts
@@ -1,4 +1,15 @@
 import { Module } from '@nestjs/common';
+import { StockController } from './stock.controller';
+import { StockService } from './stock.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Stock } from './stock.entity';
+import { Sample } from '../sample/entity/sample.entity';
+import { Taxi } from '../taxi/entity/taxi.entity';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Stock, Taxi, Sample])],
+  controllers: [StockController],
+  providers: [StockService],
+  exports: [StockService],
+})
 export class StockModule {}
--- backend/src/taxi/taxi.module.ts
+++ backend/src/taxi/taxi.module.ts
@@ -4,11 +4,11 @@ import { TaxiService } from './taxi.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Taxi } from './entity/taxi.entity';
 import { Driver } from './entity/driver.entity';
-import { StockService } from '../stock/stock.service';
+import { StockModule } from '../stock/stock.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Taxi, Driver])],
+  imports: [TypeOrmModule.forFeature([Taxi, Driver]), StockModule],
   controllers: [TaxiController],
-  providers: [TaxiService, StockService],
+  providers: [TaxiService],
 })
 export class TaxiModule {}
