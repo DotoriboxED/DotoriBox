39becc0f5b3a0231d1630074a8a3f7c47d8ddf93
Song <songyw9812@gmail.com>
2021-08-21 19:06:21 +0900

    add Customer Service

--- backend/src/customer/customer.module.ts
+++ backend/src/customer/customer.module.ts
@@ -1,4 +1,14 @@
 import { Module } from '@nestjs/common';
+import { CustomerService } from './customer.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Customer } from './customer.entity';
+import { CustomerController } from './customer.controller';
+import { StockModule } from '../stock/stock.module';
 
-@Module({})
+@Module({
+  imports: [TypeOrmModule.forFeature([Customer]), StockModule],
+  providers: [CustomerService],
+  exports: [CustomerService],
+  controllers: [CustomerController],
+})
 export class CustomerModule {}
--- backend/src/customer/customer.service.ts
+++ backend/src/customer/customer.service.ts
@@ -2,10 +2,14 @@ import { Injectable, NotFoundException } from '@nestjs/common';
 import { Repository } from 'typeorm';
 import { Customer } from './customer.entity';
 import { CustomerDto } from './customer.dto';
+import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class CustomerService {
-  constructor(private readonly customerRepository: Repository<Customer>) {}
+  constructor(
+    @InjectRepository(Customer)
+    private readonly customerRepository: Repository<Customer>,
+  ) {}
 
   async getCustomer(customerDto: CustomerDto, query: Record<string, unknown>) {
     return this.customerRepository.findOne({
@@ -16,7 +20,7 @@ export class CustomerService {
   }
 
   async createCustomer(customerDto: CustomerDto) {
-    return this.customerRepository.create(customerDto);
+    return this.customerRepository.save(customerDto);
   }
 
   async evaluate(customerDto: CustomerDto, customerId: number) {
@@ -29,5 +33,15 @@ export class CustomerService {
 
     if (!result) throw new NotFoundException();
     return result;
+  }
+
+  async mostCommonCustomer(sampleId: number) {
+    return this.customerRepository
+      .createQueryBuilder('customer')
+      .select('isMale, age')
+      .addSelect('COUNT(*) as count')
+      .where({ sampleId })
+      .groupBy('isMale, age')
+      .getRawMany();
   }
 }
