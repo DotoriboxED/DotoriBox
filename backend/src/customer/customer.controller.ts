d78999c6364fc3518d9adb72c53c16505753cd91
Song <songyw9812@gmail.com>
2021-08-21 19:07:57 +0900

    add Controller

---
+++ backend/src/customer/customer.controller.ts
@@ -0,0 +1,35 @@
+import { Body, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { CustomerService } from './customer.service';
+import { CustomerDto } from './customer.dto';
+import { StockService } from '../stock/stock.service';
+import { StockDto } from '../stock/stock.dto';
+
+@Controller('customer')
+export class CustomerController {
+  constructor(
+    private readonly customerService: CustomerService,
+    private readonly stockService: StockService,
+  ) {}
+
+  @Post('')
+  async createCustomer(
+    @Body('customer') customerDto: CustomerDto,
+    @Body('stock') stockDto: StockDto,
+  ) {
+    await this.stockService.useStock(stockDto);
+    return this.customerService.createCustomer(customerDto);
+  }
+
+  @Post(':customerId/evaluate')
+  async createEvaluate(
+    @Param('customerId') customerId: number,
+    @Body() customerDto: CustomerDto,
+  ) {
+    return this.customerService.evaluate(customerDto, customerId);
+  }
+
+  @Get('')
+  async getCustomerInfo(@Body() customerDto: CustomerDto, @Query() query) {
+    return this.customerService.getCustomer(customerDto, query);
+  }
+}
