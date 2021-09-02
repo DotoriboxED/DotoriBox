f9a9c35018805413844c03e24a0456970cb91980
Song <songyw9812@gmail.com>
2021-09-02 14:21:30 +0900

    add New Logo

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,26 +1,29 @@
 import React from 'react';
 import styled from "styled-components";
 
-import logo from './image/logo.png';
+import logo from './image/logo_new.png';
 import {BrowserRouter, Route, Switch} from "react-router-dom";
 import StartPage from './pages/customer/StartPage/View';
 import SelectPage from "@pages/customer/SelectPage/View";
+import SamplePage from '@pages/customer/SamplePage/View';
 
 const Image = styled.img`
-  height: 3rem;
+  height: 1.5rem;
   justify-content: center;
   margin: auto;
+  vertical-align: middle;
 `;
 
 const Header = styled.div`
   width:100%;
   height: 3rem;
-  background-color: #DC6E3F;
+  background-color: #C4442A;
   text-align : center;
+  line-height: 3rem;
 `;
 
 const Body= styled.div`
-  background-color:black;
+  //background-color:black;
   width:100%;
   height:calc(100vh - 3rem);
   text-align : center;
@@ -36,6 +39,7 @@ function App() {
             <Switch>
                 <Route path='/' component={StartPage} exact={true} />
                 <Route path='/selectSample' component={SelectPage} exact={true} />
+                <Route path='/userInfo' component={SamplePage} exact={true} />
             </Switch>
         </Body>
     </BrowserRouter>
