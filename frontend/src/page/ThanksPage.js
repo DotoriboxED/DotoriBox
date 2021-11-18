715deccded3a2234f9c03f897bc8e15efa5d913c
Song <songyw9812@gmail.com>
2021-11-18 15:21:18 +0900

    Edit Card Stock Error

--- frontend/src/page/ThanksPage.js
+++ frontend/src/page/ThanksPage.js
@@ -1,10 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import style from "../components/PageResource";
 import image from "../img/TestSampleImg.png";
 import Card from "../components/Card";
-import { useHistory } from "react-router-dom";
+import { useHistory, useLocation } from "react-router-dom";
 import Progressbar from "../components/Progressbar";
+import { SampleAPI } from "../API";
 
 const { Text, InputData, Footer, Button } = style;
 
@@ -55,17 +56,27 @@ const InfoText = styled.div`
   text-align: center;
   color: #bcb9b9;
   margin-top: 1.594rem;
-  margin-bottom: 2.25rem;
+  margin-bottom: 2.313rem;
   text-decoration: underline;
 `;
 
 const SubmitButton = styled(Button)`
-  margin-top: 1rem;
-  
+  margin-top: 2.25rem;
+  background-color: #bcb9b9;
 `;
 
 function ThanksPage() {
   const history = useHistory();
+  const location = useLocation();
+  const [sample, setSample] = useState();
+
+  const { sampleId, taxiId, customerId } = location.state;
+
+  useEffect(() => {
+    SampleAPI.getSample(sampleId).then((res) => {
+      setSample(res.data);
+    });
+  }, []);
 
   return (
     <Main>
@@ -76,15 +87,36 @@ function ThanksPage() {
         <br />
         기사님께 말씀드리면, 샘플을 건네주실 거에요
       </Text>
-      <Card image={image} />
+      {
+        sample &&
+        <Card
+          image={sample.image}
+          manufacture={sample.sampleInfo.manufacture}
+          name={sample.sampleInfo.name}
+          target={`
+                        ${
+            !sample.sampleTarget.age
+              ? "전연령"
+              : sample.sampleTarget.age
+          }대
+                    ${
+            sample.sampleTarget.isMale === null? '':
+            sample.sampleTarget.isMale
+              ? "남성"
+              : "여성"
+          }
+                  `}
+          stock={1}
+        />
+      }
       <Footer>
         <SampleText>
           📣 체험한 샘플을 최대 80% 저렴하게 구매하는 방법
         </SampleText>
         <TicketText>온라인 할인쿠폰을 보내드립니다</TicketText>
-        <Input placeholder='전화번호를 입력 해 주세요.' />
-        <SubmitButton>할인쿠폰 받기</SubmitButton>
-        <InfoText onClick={() => history.push("/information")} >
+        <Input placeholder="전화번호를 입력 해 주세요." />
+        <SubmitButton>아직은 쿠폰을 받을 수 없어요</SubmitButton>
+        <InfoText onClick={() => history.push("/information")}>
           휴대폰 번호 및 개인정보 수집, 이용에 동의합니다.
         </InfoText>
       </Footer>
